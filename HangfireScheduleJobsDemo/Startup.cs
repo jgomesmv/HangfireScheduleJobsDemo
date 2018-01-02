d724153d546b4812693186a61892e90693303cd7
José Gomes <jose.gomes@egplusww.com>
2018-01-02 16:07:18 +0000

    AddSolutionToRepo

---
+++ HangfireScheduleJobsDemo/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HangfireScheduleJobsDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddHangfire(x => x.UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            SetUpJobs();
+        }
+
+        private void SetUpJobs()
+        {
+            RecurringJob.AddOrUpdate("DoStuffJob", () => DoStuff(), " * * * * * "); // Minutes. Hours, Days, Months, DaysOfWeek
+        }
+
+        public void DoStuff()
+        {
+            System.Diagnostics.Debug.WriteLine($"Job example executed at: {DateTime.Now}");
+        }
+    }
+}
